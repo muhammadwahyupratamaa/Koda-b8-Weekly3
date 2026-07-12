50d4c6121584de970cac398cc60ab7869a27b4cb
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-12 21:38:44 +0700

    refactor: reorganize imports and enhance AddToCart function for better clarity and feedback

--- cart/cart.go
+++ cart/cart.go
@@ -1,6 +1,9 @@
 package cart
 
-import "koda-b8-Weekly3/menu"
+import (
+	"fmt"
+	"koda-b8-Weekly3/menu"
+)
 
 
 type CartItem struct {
@@ -9,3 +12,20 @@ type CartItem struct {
 }
 
 var cart []CartItem
+
+func AddToCart(menuItem menu.Menu, qty int) {
+	for i := range cart {
+		if cart[i].Menu.ID == menuItem.ID {
+			cart[i].Quantity += qty
+			fmt.Println("Quantity updated successfully!")
+			return
+		}
+	}
+
+	cart = append(cart, CartItem{
+		Menu:     menuItem,
+		Quantity: qty,
+	})
+
+	fmt.Println("Menu successfully added to cart!")
+}
